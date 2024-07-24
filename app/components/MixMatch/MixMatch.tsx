6a891ac80c8755fcfed9ac6f47188ef92e0a2cbf
hirent10 <hirentariwala10@gmail.com>
2024-07-24 14:54:54 +0530

    fix: mix-and-match page bug fix

--- app/components/MixMatch/MixMatch.tsx
+++ app/components/MixMatch/MixMatch.tsx
@@ -1,5 +1,5 @@
-import {Image} from '@shopify/hydrogen';
 import React, {useEffect, useRef, useState} from 'react';
+import DownArrow from '~/assets/Icons/DownArrow';
 import Slider from '../SliderAnimation/SliderAnimation';
 import type {MixMatchProps} from './types';
 
@@ -27,8 +27,19 @@ const optionsData = {
     'Clover',
     'Alphabet',
   ],
-  Bag: ['Pouch', 'Boxie'],
-  Pouches: ['Tulip', 'Boxie'],
+  Bag: [
+    'Daisy',
+    'Iris',
+    'Poppy',
+    'Lily',
+    'Tulip',
+    'Party',
+    'Jolly',
+    'Dimsum',
+    'Bossy',
+    'Journey',
+  ],
+  Pouches: ['Boxie', 'Love', 'Round'],
 } as {
   Straps: string[];
   Pouches: string[];
@@ -84,23 +95,27 @@ const MixMatch = ({type}: MixMatchProps) => {
         <div
           role="button"
           tabIndex={0}
-          className="select bg-black text-white flex justify-between items-center rounded-full md:px-7 px-2 md:py-2 py-1 md:mb-5 mb-1 transition-all duration-300 md:text-[32px] text-[9px] w-full font-MontserratBold capitalize cursor-pointer"
+          className={`select bg-black text-white md:border-[3px] border-[1px] border-black flex justify-between items-center rounded-full md:px-7 px-2 md:py-1.5 py-0 md:mb-5 mb-1 transition-all duration-300 md:text-[32px] text-[9px] w-full md:font-MontserratBold font-MontserratSemiBold capitalize cursor-pointer ${
+            isMenuOpen ? '!bg-[#f4eded] !text-black' : ''
+          }`}
           onClick={handleSelectClick}
           onKeyDown={(e) => handleKeyDown(e as any, '')}
         >
           {selectedOption || type}
-          <div className="md:w-9 w-2.5 md:h-9 h-2.5 bg-softPeach flex justify-center items-center rounded-full pointer-events-none">
-            <Image
-              srcSet="/icons/downArrow.svg"
-              className={`transition-all duration-300 pointer-events-none rotate-90 md:w-5 w-1 ${
-                isMenuOpen ? '!rotate-0' : ''
+          <div
+            className={`md:w-9 w-2.5 md:h-9 h-2.5 bg-softPeach flex justify-center items-center rounded-full pointer-events-none ${
+              isMenuOpen ? '!bg-black' : ''
+            }`}
+          >
+            <DownArrow
+              className={`text-black transition-all duration-300 pointer-events-none rotate-90 md:w-5 w-1.5 ${
+                isMenuOpen ? 'text-white !rotate-0' : ''
               }`}
-              width={20}
             />
           </div>
         </div>
         <ul
-          className={`absolute w-full top-16 left-2/4 text-2xl opacity-0 hidden bg-black text-white rounded-[19px] transform -translate-x-1/2 transition duration-200 z-10 font-MontserratRegular ${
+          className={`absolute w-full md:top-16 top-4 left-2/4 text-2xl opacity-0 hidden bg-black text-white md:rounded-[19px] rounded-lg transform -translate-x-1/2 transition duration-200 z-10 font-MontserratRegular ${
             isMenuOpen ? '!block !opacity-100' : ''
           }`}
         >
@@ -113,7 +128,7 @@ const MixMatch = ({type}: MixMatchProps) => {
               tabIndex={index}
               onClick={() => handleOptionClick(option)}
               onKeyDown={(e) => handleKeyDown(e, option)}
-              className="hover:bg-lightGreen py-2 px-10 rounded-[19px] cursor-pointer"
+              className="hover:bg-lightGreen md:py-2 py-0.5 md:px-10 px-3 md:rounded-[19px] rounded-lg cursor-pointer md:text-2xl text-[10px] leading-normal"
             >
               {option}
             </li>
